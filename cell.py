e246d9290f0ad65d659fb32e9b84e157ce62a922
HenningRixen <henning.rixen@freenet.de>
2024-03-14 17:30:50 +0100

    starting cell

---
+++ cell.py
@@ -0,0 +1,10 @@
+class Cell:
+    def __init__(self, win):
+        self.has_left_wall = has_left_wall
+        self.has_right_wall = has_right_wall
+        self.has_top_wall = has_top_wall
+        self.has_bottom_wall = has_bottom_wall
+        self._x1 = _x1
+        self._x2 = _x2
+        self._y1 = _y1
+        self._y2 = _y2
