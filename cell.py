da7071c965569280d0ffcd5529c3fb26a84bdbf0
HenningRixen <henning.rixen@freenet.de>
2024-03-15 12:27:13 +0100

    cell works

--- cell.py
+++ cell.py
@@ -1,10 +1,35 @@
+from graphics import Line, Point
+
+
 class Cell:
     def __init__(self, win):
-        self.has_left_wall = has_left_wall
-        self.has_right_wall = has_right_wall
-        self.has_top_wall = has_top_wall
-        self.has_bottom_wall = has_bottom_wall
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = win
+
+    def draw(self, _x1, _y1, _x2, _y2):
         self._x1 = _x1
         self._x2 = _x2
         self._y1 = _y1
         self._y2 = _y2
+
+        if self.has_left_wall:
+            left_wall = Line(Point(_x1, _y1), Point(_x1, _y2))
+            self._win.draw_line(left_wall)
+        if self.has_top_wall:
+            top_wall = Line(Point(_x1, _y1), Point(_x2, _y1))
+            self._win.draw_line(top_wall)
+        if self.has_right_wall:
+            right_wall = Line(Point(_x2, _y1), Point(_x2, _y2))
+            self._win.draw_line(right_wall)
+        if self.has_bottom_wall:
+            bottom_wall = Line(Point(_x1, _y2), Point(_x2, _y2))
+            self._win.draw_line(bottom_wall)
+
+
--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-from graphics import Window, Point, Line
+from graphics import Window
+from cell import Cell
 
 def main():
     win = Window(800, 600)
-    line = Line(Point(50,50), Point(400,400))
-    line2 = Line(Point(500,500), Point(555,10))
-    win.draw_line(line, "black")
-    win.draw_line(line2, "red")
+
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50, 50, 120, 120)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(150, 150, 200, 200)
+
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(250, 250, 275, 275)
+
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(300, 300, 500, 500)
+
+
+
+    
+    
+
     win.wait_for_close()
 
 main()
